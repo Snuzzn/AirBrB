5251d7e56c32f9058de464fbbfd1ba3f29400738
Sanojan Thiyagaraja <sano@Macbook.local>
2021-10-29 15:23:49 +1100

    added alt tags for accessibility

--- frontend/src/components/Dropdown.jsx
+++ frontend/src/components/Dropdown.jsx
@@ -4,18 +4,18 @@ import PropTypes from 'prop-types';
 
 export default function Dropdown ({ title, options }) {
   return (
-      <Menu>
-        <MenuButton>
-          {title}
-        </MenuButton>
-          <MenuList className="bg-gray-100 p-4 rounded-lg animate-wiggle">
-              {options.map((item, index) => <MenuItem key={index}
-              className="hover:bg-gray-200 flex gap-2 items-center cursor-pointer p-3 rounded">
-              {item.icon}
-              {item.text}
-              </MenuItem>)}
-          </MenuList>
-      </Menu>
+    <Menu>
+      <MenuButton>
+        {title}
+      </MenuButton>
+        <MenuList className="bg-gray-100 p-4 rounded-lg animate-wiggle">
+            {options.map((item, index) => <MenuItem key={index}
+            className="hover:bg-gray-200 flex gap-2 items-center cursor-pointer p-3 rounded">
+            {item.icon}
+            {item.text}
+            </MenuItem>)}
+        </MenuList>
+    </Menu>
   );
 }
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,11 +5,11 @@ import { AiOutlineUserAdd, AiOutlineLogin } from 'react-icons/ai'
 import { HiUserCircle } from 'react-icons/hi'
 
 export default function Navbar () {
-  const userOptions = [{ icon: <AiOutlineUserAdd/>, text: 'Register' }, { icon: <AiOutlineLogin/>, text: 'Login' }]
-  const userIcon = <HiUserCircle size="2em" className="text-gray-600 "/>
+  const userOptions = [{ icon: <AiOutlineUserAdd alt="Register icon: user being added"/>, text: 'Register' }, { icon: <AiOutlineLogin alt="Login icon: arrow entering circle"/>, text: 'Login' }]
+  const userIcon = <HiUserCircle size="2em" className="text-gray-600" alt="genericUserIcon"/>
   return (
     <nav className="bg-white shadow-md px-2 sm:px-6 lg:px-8 flex items-center justify-between h-16">
-      <img className="mr-auto lg:block h-7 w-auto" src={logo} alt="logo: eye with house inside"/>
+      <img className="mr-auto lg:block h-7 w-auto" src={logo} alt="AirBrB logo: eye with house inside iris"/>
       <Dropdown title={userIcon} options={userOptions}/>
     </nav>
   );
