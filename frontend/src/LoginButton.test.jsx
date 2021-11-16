b5a2e4c968f798b5938b5aa189fffcd428070653
Adam Brieger <adam.brieger@gmail.com>
2021-11-16 12:52:23 +1100

    Adds component testing for LoginButton and additional commenting

---
+++ frontend/src/LoginButton.test.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import LoginButton from './components/LoginButton';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
+import { AiOutlineArrowRight } from 'react-icons/ai';
+
+describe('LoginButton', () => {
+  /**
+   * LoginButton takes in a click function and optional text 
+   * to display. It is used across multiple components, 
+   * including for login and registration.
+   */
+
+  const noop = () => {};
+  
+  it('triggers onClick event handler when clicked', () => {
+    const onClick = jest.fn();
+    shallow(<LoginButton handleClick={onClick} />).simulate('click');
+    expect(onClick).toHaveBeenCalledTimes(1);
+  })
+
+  it('uses custom title', () => {
+    const customText = 'custom text';
+    const button = shallow(<LoginButton 
+      onClick={noop} 
+      buttonText={customText} 
+    />);
+    // Button contains one div to hold the custom text
+    expect(button.find('div').text()).toBe(customText);
+  })
+
+  it('uses default title if no custom text provided', () => {
+    const button = shallow(<LoginButton 
+      onClick={noop} 
+    />);
+    // Button's default text is 'Submit'
+    expect(button.find('div').text()).toBe('Submit');
+  })
+
+  it('contains icon indicating submission', () => {
+    const button = shallow(<LoginButton 
+      onClick={noop} 
+    />);
+    expect(button.contains(<AiOutlineArrowRight />))
+  })
+
+  it('icon has alt prop for accessibility', () => {
+    const button = shallow(<LoginButton 
+      onClick={noop} 
+    />);
+    expect(button.find(<AiOutlineArrowRight />).find('alt')).toBeDefined();
+  })
+
+  // Snapshots
+  it('renders with minimal props', () => {
+    const button = renderer.create(<LoginButton 
+      onClick={noop} 
+    />).toJSON();
+    expect(button).toMatchSnapshot();
+  })
+
+  it('renders with provided text', () => {
+    const button = renderer.create(<LoginButton 
+      onClick={noop}
+      type='I am a button!'
+    />).toJSON();
+    expect(button).toMatchSnapshot();
+  })
+})
--- frontend/src/TextInput.test.jsx
+++ frontend/src/TextInput.test.jsx
@@ -5,84 +5,91 @@ import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
 
 describe('TextInput', () => {
-    const noop = () => {};
+  /**
+   * TextInput captures user input in login and register. It takes
+   * in an onChange function, the type of input, an optional placeholder, 
+   * and a custom aria-label for accessibility.
+   */
+  
+  const noop = () => {};
 
-    it('triggers onChange event on user input', () => {
-        const onChange = jest.fn();
-        const inputField = shallow(<TextInput 
-            type='text'
-            setState={onChange} 
-        />);
-        const event = {target: { value: 'a' }}
-        inputField.simulate('change', event);
-        expect(onChange).toHaveBeenCalledTimes(1);
-    })
+  it('triggers onChange event on user input', () => {
+    const onChange = jest.fn();
+    const inputField = shallow(<TextInput 
+        type='text'
+        setState={onChange} 
+    />);
+    const event = {target: { value: 'a' }}
+    inputField.simulate('change', event);
+    expect(onChange).toHaveBeenCalledTimes(1);
+  })
 
-    it('captures user input on change', () => {
-        const onChange = jest.fn();
-        const inputField = shallow(<TextInput 
-            type='text'
-            setState={onChange} 
-        />);
-        const event = {target: { value: 'testing this value' }}
-        inputField.simulate('change', event);
-        expect(onChange).toHaveBeenCalledWith('testing this value');
-    })
+  it('captures user input on change', () => {
+    const onChange = jest.fn();
+    const inputField = shallow(<TextInput 
+        type='text'
+        setState={onChange} 
+    />);
+    const event = {target: { value: 'testing this value' }}
+    inputField.simulate('change', event);
+    expect(onChange).toHaveBeenCalledWith('testing this value');
+  })
 
-    it('aria-label is defined with prop value', () => {
-        const inputField = shallow(<TextInput 
-            label='Test Label!' 
-            setState={noop} 
-        />);
-        expect(inputField.prop('aria-label')).toBe('Test Label!');
-    })
+  it('aria-label is defined with prop value', () => {
+    const inputField = shallow(<TextInput 
+      label='Test Label!' 
+      setState={noop} 
+    />);
+    expect(inputField.prop('aria-label')).toBe('Test Label!');
+  })
 
-    it('type is defined with prop value', () => {
-        const inputField = shallow(<TextInput 
-            type='text'
-            setState={noop} 
-        />);
-        expect(inputField.prop('type')).toBe('text');
-    })
+  it('type is defined with prop value', () => {
+    const inputField = shallow(<TextInput 
+      type='password'
+      setState={noop} 
+    />);
+    expect(inputField.prop('type')).toBe('password');
+  })
 
-    it('placeholder is defined with prop value', () => {
-        const inputField = shallow(<TextInput 
-            type='text'
-            placeholder='Test Placeholder!'
-            setState={noop} 
-        />);
-        expect(inputField.prop('placeholder')).toBe('Test Placeholder!');
-    })
+  it('placeholder is defined with prop value', () => {
+    const inputField = shallow(<TextInput 
+      type='text'
+      placeholder='Test Placeholder!'
+      setState={noop} 
+    />);
+    expect(inputField.prop('placeholder')).toBe('Test Placeholder!');
+  })
 
-    it('uses placeholder text defined in prop', () => {
-        render(<TextInput 
-            type='text'
-            placeholder='Placeholder!'
-            setState={noop} 
-        />);
-        expect(screen.getByPlaceholderText('Placeholder!')).toBeInTheDocument();
-    })
+  it('uses placeholder text defined in prop', () => {
+    render(<TextInput 
+      type='text'
+      placeholder='Placeholder!'
+      setState={noop} 
+    />);
+    expect(screen.getByPlaceholderText('Placeholder!')).toBeInTheDocument();
+  })
 
-    it('uses default placeholder text if no prop passed in', () => {
-        render(<TextInput 
-            type='text'
-            setState={noop} 
-        />);
-        expect(screen.getByPlaceholderText('Please enter details')).toBeInTheDocument();
-    })
+  it('uses default placeholder text if no prop passed in', () => {
+    render(<TextInput 
+      type='text'
+      setState={noop} 
+    />);
+    // TextInput's default placeholder is 'Please enter details'
+    expect(screen.getByPlaceholderText('Please enter details')).toBeInTheDocument();
+  })
 
-    // Snapshots
-    it('renders with minimal props', () => {
-        const inputField = renderer.create(<TextInput onChange={noop} />).toJSON();
-        expect(inputField).toMatchSnapshot();
-    })
+  // Snapshots
+  it('renders with minimal props', () => {
+    const inputField = renderer.create(<TextInput onChange={noop} />).toJSON();
+    expect(inputField).toMatchSnapshot();
+  })
 
-    it('renders with provided placeholder and type', () => {
-        const inputField = renderer.create(<TextInput 
-            onChange={noop}
-            type='text'
-            placeholder='placeholder test'
-        />).toJSON();
-        expect(inputField).toMatchSnapshot();
-    })
+  it('renders with provided placeholder and type', () => {
+    const inputField = renderer.create(<TextInput 
+      onChange={noop}
+      type='text'
+      placeholder='placeholder test'
+    />).toJSON();
+    expect(inputField).toMatchSnapshot();
+  })
 })
